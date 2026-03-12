ecc952d9de38819a464b7176fe80bea4ca4a7a3d
Jitendra Singh <jitendrasingh108108@gmail.com>
2026-03-12 23:04:11 +0530

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("govdata.csv")
+
+# text column remove
+df = df.drop(columns=["State/UT"], errors="ignore")
+df = df.drop(columns=["S. No."], errors="ignore")
+
+# correlation heatmap
+plt.figure(figsize=(12,8))
+sns.heatmap(df.corr(numeric_only=True), cmap="coolwarm")
+
+plt.title("Road Accident Feature Correlation")
+plt.show()
+=
